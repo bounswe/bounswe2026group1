a06cd5d85983c98e8762cf3e8c8f39281e563eec
burakaltunay <burakaltunay11235@gmail.com>
2026-03-12 14:40:05 +0300

    Revert "feat: add Burak button and API integration"
    This reverts commit 7bc468b3b5cbe1090377efdb96375c1b27504a8a.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
  * Update BUTTON_LABELS to set the text displayed on each button.
  */
 
-const NUM_BUTTONS = 8; 
+const NUM_BUTTONS = 5;
 
 const BUTTON_LABELS = [
   "Button 1",
@@ -13,7 +13,4 @@ const BUTTON_LABELS = [
   "Button 3",
   "Button 4",
   "Button 5",
-  "Button 6", 
-  "Button 7", 
-  "Burak Altunay", 
 ];
