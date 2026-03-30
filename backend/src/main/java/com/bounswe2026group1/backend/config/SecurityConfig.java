72328e30dd13c3e40896978de131ea0a6c7d9036
Efe Mantaroğlu <efemantaroglum@gmail.com>
2026-03-30 14:36:11 +0300

    fix: change allowed CORS to localhost only for security

--- backend/src/main/java/com/bounswe2026group1/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/bounswe2026group1/backend/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:[*]"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
