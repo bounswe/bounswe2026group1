3d65261e8c21ed736bb670b97e2baee1f3c6b494
Efe Mantaroğlu <efemantaroglum@gmail.com>
2026-04-07 14:01:45 +0300

    fix: add merged handler

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -417,7 +417,13 @@ function Home() {
                 key={report.id}
                 position={[report.latitude, report.longitude]}
                 icon={makeMarkerIcon(report.status, report.tags[0])}
-                eventHandlers={{ click: () => setSelectedReportId(report.id) }}
+                eventHandlers={{
+                  click: () => {
+                    setShowCreatePanel(false)
+                    setNewReportPin(null)
+                    setSelectedReportId(report.id)
+                  },
+                }}
               />
             ))}
             {newReportPin && (
