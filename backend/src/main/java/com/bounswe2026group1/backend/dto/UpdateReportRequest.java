1ec27a64a196b9845f6fe845b24df8a0e734f541
alaaddinerenn <nealaadin@gmail.com>
2026-04-30 23:59:13 +0300

    feat: create UpdateReportRequest with tag, location and mediaIdsToRemove

---
+++ backend/src/main/java/com/bounswe2026group1/backend/dto/UpdateReportRequest.java
@@ -0,0 +1,19 @@
+package com.bounswe2026group1.backend.dto;
+
+import com.bounswe2026group1.backend.model.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateReportRequest {
+    private String description;
+    private Tag tag;
+    private double latitude;
+    private double longitude;
+    private List<Long> mediaIdsToRemove;
+}
